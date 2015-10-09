57954270ae7a931f6b99aa120c2b85df2bc351b8
hoter <aytpaev@gmail.com>
2015-10-09 17:00:16 +0600

    Initialize

---
+++ src/TwitterBot.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace TwitterBot;
+
+class TwitterBot {
+  private $connection;
+
+  public function __construct($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret) {
+    $this->connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+  }
+
+  public function retweet($since = -1, $count = 1) {
+   if ($since === -1) {
+     $since = $this->findLastTweet();
+   }
+
+   
+  }
+
+  public function findLastTweet() {
+    $message = $this->connection->get("statuses/user_timeline", array('count' => 1));
+    print_r($message);
+  }
+}
