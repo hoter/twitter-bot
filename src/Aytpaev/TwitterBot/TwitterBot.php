bf91eab4c034875d85d628d2a5c42af1e24d9bd5
Erlan Aytpaev <aytpaev@gmail.com>
2015-10-14 15:14:36 +0500

    Add "favoriteByHashTag" function

--- src/Aytpaev/TwitterBot/TwitterBot.php
+++ src/Aytpaev/TwitterBot/TwitterBot.php
@@ -27,6 +27,23 @@ class TwitterBot {
    return FALSE;
   }
 
+  public function favoriteByHashTag($hashtag, $since = -1, $count = 1) {
+   if ($since < 0) {
+     $since = $this->findLastTweet();
+   }
+
+   $tweets = $this->connection->get("search/tweets", array('q' => '#' . $hashtag, 'result_type' => 'mixed', 'since_id' => $since + 1, 'count' => $count));
+   if (!empty($tweets->statuses)) {
+     foreach ($tweets->statuses as $tweet) {
+       print_r($this->connection->post("favorites/create", array('id' => $tweet->id)));
+     }
+
+     return TRUE;
+   }
+
+   return FALSE;
+  }
+
   protected function findLastTweet() {
     $messages = $this->connection->get("statuses/user_timeline", array('count' => 1));
     if ($messages) {
